c93c5781d013112f1a11badd3c35bf40638cfef0
Dustin Wurtz <dtwurtz@gmail.com>
2018-06-06 15:32:43 -0700

    Cleaned clickCrl.js and clickStorage.js

--- app/clickCtrl.js
+++ app/clickCtrl.js
@@ -30,55 +30,6 @@ angular.module('app', ['naif.base64']).controller('clickCtrl', function ($scope,
 
     $scope.submit = function() {
 
-      var uploadedImages = $scope.uploadedImage;
-      var finalHtml = { };
-      var groupArrayHtml = $scope.rawHtml.split("\n");
-      var htmlAndImages = { };
-      var imageDataArray = [ ];
-      // ^^^^^^^^^ Testing rawHtml straight to be sent to RI ^^^^^^^^^
-      var rawHtml = $scope.rawHtml;
-
-      console.log(imageDataArray)
-      // ******* JS logic to turn raw HTML to be used in RI *********
-      for(var i = 0; i < groupArrayHtml.length; i++) {
-          var singleArrayHtml = groupArrayHtml[i].split("");
-          var arrayLength = singleArrayHtml.length;
-
-          singleArrayHtml = singleArrayHtml.join('');
-
-          groupArrayHtml[i] = singleArrayHtml.trim();
-          arrayLength = 0;
-          singleArrayHtml = { };
-      }
-
-      finalHtml = groupArrayHtml.join('');
-
-      $scope.showHtml = finalHtml;
-
-
-
-      // ******* JSON Object to be sent over to clickStorage after images and HTML are ready for transport *****
-      for(var i = 0; i < uploadedImages.length; i++) {
-          var sampleArray = {
-              "itemData": uploadedImages[i].base64
-          };
-
-          imageDataArray.push(sampleArray);
-      };
-
-      htmlAndImages = {
-          html: {
-              "documentPath": "/contentlibrary/donut_2/" + $scope.htmlFileName + ".htm",
-              "content": rawHtml
-          },
-          images: imageDataArray,
-          htmlFileName: $scope.htmlFileName
-      };
-
-      console.log("Below should be the json object with the html:");
-      console.log(htmlAndImages);
-
-      clickStorage.createCampaign(htmlAndImages);
-      $scope.newSubject = '';
+        clickStorage.createCampaign();
     }
 });
