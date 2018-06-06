07cd7600304fd5764ee0c785a127496f6fac9be3
Dustin Wurtz <dtwurtz@gmail.com>
2018-06-06 16:30:43 -0700

    Initial implementation of adding in the mailchimp api code

--- app/clickStorage.js
+++ app/clickStorage.js
@@ -20,6 +20,36 @@ angular.module('app').service('clickStorage', function ($q, $http) {
     // ******* Create Campaign *******
     this.createCampaign = function (data) {
         console.log(data);
+        var campaignId = "";
+        var campaignSetUp = {
+            method: 'POST',
+            url: "https://us15.api.mailchimp.com/3.0/campaigns",
+            headers: {
+                "Authorization": "Basic ZHVzdGluLnd1cnR6QG9yYWNsZS5jb206ZDdhYjc0ZmE2YzgxYjc2ODg3MWQ1MDAzMDhhZTVhY2YtdXMxNQ==",
+                "Content-type": "application/json"
+            },
+            data: {
+                type: "regular",
+                recipients: {
+                    list_id: "bd56aae19c"
+                },
+                settings: {
+                    subject_line: data.subjectLine,
+                    title: data.subjectLine,
+                    from_name: "Dustin Wurtz",
+                    reply_to: "dustin.wurtz@oracle.com",
+                    auto_footer: false
+                }
+            }
+        }
+
+        $http(campaignSetUp).then(function successCallback(response) {
+            console.log(response);
+        },
+        function errorCallback(response) {
+            console.log(response);
+            clickStorage.errorPage = true;
+        });
     }
 
 });
